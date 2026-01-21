2d6121c35a05831bee0b8af88d80c65ad3494e78
Carlos <carlosfvargas526@gmail.com>
2026-01-21 00:13:02 -0800

    fonts

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: `'Zalando Sans', system-ui, -apple-system, BlinkMacSystemFont,
+                 'Segoe UI', Roboto, Helvetica, Arial, sans-serif`,
+  },
+});
+
+export default theme;
+
